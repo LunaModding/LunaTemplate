89a01e5419e1a39b881e959fbfde29a43c01f579
Millzy <64277238+MillzyDev@users.noreply.github.com>
2023-01-22 15:04:33 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,11 @@
+ADDON_METADATA = {
+    _lunaVersion="$lunaVersion$",
+    id="$id$",
+    name="$name$",
+    author="$author$",
+    version="$version$"
+}
+
+function init() {
+    luna.log("$name$ initialised successfully!")
+end
